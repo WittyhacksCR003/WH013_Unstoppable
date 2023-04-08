2dfbcef4be4730be5f76db10a8e3e9f8ae6d8180
Anekant <Anekantjainsagar@gmail.com>
2023-04-08 05:46:53 +0530

    Added employee side frontend with responsivenss

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import VideoInput from "./Component/videoDisplay";
 
 const App = () => {
   return (
-    <div>
-      <p className="text-5xl text-red-500">Hello world</p>
+    <div className="flex items-center justify-center flex-col h-[100vh]">
+      <p className="text-center text-2xl my-2 font-bold">
+        Upload the Video here
+      </p>
+      <VideoInput />
     </div>
   );
 };
